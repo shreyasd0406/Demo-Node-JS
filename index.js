a9533a6ae13a1d22681f897c557419b110ae8122
shreyasd0406 <107049971+shreyasd0406@users.noreply.github.com>
2023-07-06 18:35:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require('express');
+var express = require('express');
+var app = express();
 
-const app = express();
-const port = 3005;
+app.get('/', function (req, res) {
+    res.send('{ "response": "Hello From Thetips4you" }');
+});
 
-app.listen(port, ()=> {
-    console.log("Listeneing to port - ", port)
-})
+app.get('/will', function (req, res) {
+    res.send('{ "response": "Hello World" }');
+});
+app.get('/ready', function (req, res) {
+    res.send('{ "response": " Great!, It works!" }');
+});
+app.listen(process.env.PORT || 3000);
+module.exports = app;
